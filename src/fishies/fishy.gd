73d02710932fefd4e669ed9e79c19b5a28038beb
Ángela López <angela.lbsr@gmail.com>
2022-01-16 18:33:32 +0100

    path follow flips fishy when walking the path back

--- src/fishies/fishy.gd
+++ src/fishies/fishy.gd
@@ -1,3 +1,9 @@
 extends Area2D
 class_name Fish
 
+
+onready var sprite: Sprite = $Sprite
+
+
+func flip() -> void:
+	sprite.flip_h = not sprite.flip_h
--- src/fishies/path_following_fishy/path_following_fishy.gd
+++ src/fishies/path_following_fishy/path_following_fishy.gd
@@ -2,8 +2,39 @@ extends PathFollow2D
 
 
 export var speed: int = 100
+export var back: bool = false
 
+
+onready var path: Path2D = get_parent()
+onready var total_length:= path.curve.get_baked_length()
+
+
+onready var fishy: Fish = $Fishy
+
+
+func _ready() -> void:
+	if back:
+		fishy.flip()
+		
 
 func _physics_process(delta: float) -> void:
-	offset += speed * delta
+	var movement = speed * delta
+	
+	if back:
+		if offset < movement:
+			offset = movement - offset
+			flip()
+		else:
+			offset -= movement
+	else:
+		if total_length - offset < movement:
+			offset = 2 * total_length - offset - movement
+			flip()
+		else: 
+			offset += movement
+	
+
+func flip() -> void:
+	back = not back
+	fishy.flip()
 	
