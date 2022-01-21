803c2f483af1e261678be5c5d0152b3d79ee407b
Ángela López <angela.lbsr@gmail.com>
2022-01-21 22:48:55 +0100

    touch event shoot

--- src/player/harpoon/states/base.gd
+++ src/player/harpoon/states/base.gd
@@ -5,12 +5,13 @@ onready var harpoon = find_parent("Harpoon")
 
 
 func handle_input(event: InputEvent) -> void:
-	if Input.is_action_just_pressed("reelin"):
+	if Input.is_action_pressed("reelin"):
 		reel_in()
 	elif Input.is_action_just_pressed("shoot"):
 		shoot()
-
-
+	elif event is InputEventScreenTouch and event.is_pressed():
+		shoot()
+		
 func shoot() -> void:
 	pass
 
